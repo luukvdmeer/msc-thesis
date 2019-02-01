28a9ac607b53544d769c506d00b03e40046cbcfe
luukvdmeer <luukvandermeer@live.nl>
2019-02-01 11:28:30 +0100

    Updated workflow

--- Workflow Scripts/workflow.R
+++ Workflow Scripts/workflow.R
@@ -19,21 +19,6 @@ gridcentroids = gridcells %>%
   sf::st_centroid() %>%
   sf::st_transform(crs = 4326)
 
-# Load distance data for grid cell centroids during training period
-distancedata_centroids_train = readRDS('RDS Files/distancedata_centroids_train.rds')
-
-# Cluster
-clusters = dockless::spatial_cluster(
-  data = distancedata_centroids_train,
-  grid = gridcells,
-  K = 4,
-  omega = seq(0, 1, 0.1)
-)
-
-# Add cluster information to grid cells and grid cell centroids
-gridcells$cluster     = clusters$indices
-gridcentroids$cluster = clusters$indices
-
 # Load usage data during training period
 usagedata_train = readRDS('RDS Files/usagedata_train.rds')
 
@@ -54,6 +39,22 @@ gridcells$intensity = dockless::usage_intensity(
 # Add intensity information to grid cell centroids
 gridcentroids$intensity = gridcells$intensity
 
+# Load distance data for grid cell centroids during training period
+distancedata_centroids_train = readRDS('RDS Files/distancedata_centroids_train.rds')
+
+# Cluster
+clusters = dockless::spatial_cluster(
+  data = distancedata_centroids_train,
+  grid = gridcells,
+  area = systemarea,
+  K = 4,
+  omega = seq(0, 1, 0.1)
+)
+
+# Add cluster information to grid cells and grid cell centroids
+gridcells$cluster     = clusters$indices
+gridcentroids$cluster = clusters$indices
+
 # Create model points
 modelpoints = dockless::create_modelpoints(
   centroids = gridcentroids
@@ -73,13 +74,58 @@ models = dockless::build_models(
 
 ## ------------------------ FORECAST LOOP ---------------------------
 
+## OPERATOR PERSPECTIVE
+# Load distance data for grid cell centroids during test period
+distancedata_centroids_test = readRDS('distancedata_centroids_test.rds')
+
+# Forecast with DBAFS
+t_start = Sys.time()
+
+forecasts_operator_dbafs = dockless::forecast_multiple(
+  data = distancedata_centroids_test,
+  method = 'DBAFS',
+  perspective = 'operator',
+  points = gridcentroids,
+  models = models
+)
+
+t_end = Sys.time()
+duration_operator_dbafs = t_end - t_start
+
+# Forecast with NFS
+t_start = Sys.time()
+
+forecasts_operator_nfs = dockless::forecast_multiple(
+  data = distancedata_centroids_test,
+  method = 'NFS',
+  perspective = 'operator',
+  points = gridcentroids
+)
+
+t_end = Sys.time()
+duration_operator_nfs = t_end - t_start
+
+t_start = Sys.time()
+
+# Forecast with EFS
+t_start = Sys.time()
+
+forecasts_operator_efs = dockless::forecast_multiple(
+  data = distancedata_centroids_test,
+  method = 'EFS',
+  perspective = 'operator',
+  points = gridcentroids
+)
+
+t_end = Sys.time()
+duration_operator_efs = t_end - t_start
+
+t_start = Sys.time()
+
+
+## USER PERSPECTIVE
 # Load usage data during test period
 usagedata_test = readRDS('RDS Files/usagedata_test.rds')
 
-# Create test points
-testpoints = dockless::create_testpoints(
-  data = usagedata_test,
-  area = systemarea,
-  clusters = clusters$outlines,
-  n = 1743
-)
+# Load test points
+testpoints = readRDS('RDS Files/testpoints.rds')
